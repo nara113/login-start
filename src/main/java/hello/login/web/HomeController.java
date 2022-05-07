47a38d3d5406128904af8b244cd853f25f676ced
nara113 <0554my@naver.com>
2022-05-08 03:52:43 +0900

    로그인 처리1 - 쿠키, 세션 - 로그인 처리하기 - 서블릿 HTTP 세션2

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -59,7 +60,7 @@ public class HomeController {
     }
 
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String loginHomeV3(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
 
@@ -68,6 +69,19 @@ public class HomeController {
         }
 
         Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String loginHomeV3Spring(
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) Member member,
+            Model model) {
 
         if (member == null) {
             return "home";
